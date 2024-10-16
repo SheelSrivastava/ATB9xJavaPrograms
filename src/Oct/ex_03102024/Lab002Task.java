380d67eba428479ef51b8007b0d09930549525c9
Dell <Srivastavasheel@gmail.com>
2024-10-16 20:38:28 +0530

    This the file

---
+++ src/Oct/ex_03102024/Lab002Task.java
@@ -0,0 +1,20 @@
+package Oct.ex_03102024;
+
+public class Lab002Task {
+    public static void main(String[] args) {
+        //Table of 10
+        System.out.println(10*1);
+        System.out.println(10*2);
+        System.out.println(10*3);
+        System.out.println(10*4);
+        System.out.println(10*5);
+        System.out.println(10*6);
+        System.out.println(10*7);
+        System.out.println(10*8);
+        System.out.println(10*9);
+        System.out.println(10*10);
+
+    }
+
+    }
+
---
+++ src/Oct/ex_03102024/Lab002task002.java
@@ -0,0 +1,11 @@
+package Oct.ex_03102024;
+
+public class Lab002task002 {
+    public static void main(String[] args) {
+        // Create calculator
+        System.out.println(10+1);
+        System.out.println(10-1);
+        System.out.println(10*2);
+        System.out.println(10/2);
+    }
+}
